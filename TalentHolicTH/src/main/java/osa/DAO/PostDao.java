4b2480097e41caf73fc052aea67b5a6fc1836b92
BrayanAbraham <brayanabraham@gmail.com>
2018-09-02 15:48:09 +0530

    First Commit
    uploading all files

---
+++ TalentHolicTH/src/main/java/osa/DAO/PostDao.java
@@ -0,0 +1,39 @@
+package osa.DAO;
+
+import osa.entity.PostEntity;
+import static osa.services.OfyService.ofy;
+
+import java.util.List;
+
+public class PostDao {
+
+	
+	public boolean checkIfExist(long id) {
+		
+		PostEntity pe=ofy().load().type(PostEntity.class).id(id).now();
+		
+		if(pe!=null)
+			return true;
+		else
+			return false;
+	}
+
+	public void uploadPost(PostEntity pe) {
+
+		ofy().save().entity(pe).now();
+		
+	}
+	
+	public List<PostEntity> list()
+	{
+		return ofy().load().type(PostEntity.class).list();
+	}
+
+	public void deletePost(String id) {
+		// TODO Auto-generated method stub
+		
+		long in=Long.parseLong(id);
+		ofy().delete().type(PostEntity.class).id(in);
+	}
+
+}
